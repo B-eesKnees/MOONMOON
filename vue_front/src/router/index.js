19491c4d547c10ea3012978fac8bbc6efb08d77b
jb3757 <bj3757@gmail.com>
2023-08-01 17:11:41 +0900

    08/01

--- vue_front/src/router/index.js
+++ vue_front/src/router/index.js
@@ -1,44 +1,50 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Main from "../views/Main.vue"
-import Login from "../views/Login.vue"
-import Join from "../views/Join.vue"
+import Main from "../views/Main.vue";
+import Login from "../views/Login.vue";
+import Join from "../views/Join.vue";
 
 import booktestPage from "../views/bookTest.vue";
 import cart from "../views/cart.vue";
+import resultpage from "../views/paymentResult.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Main",
-        component: Main,
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    },
-    {
-        path: "/join",
-        name: "Join",
-        component: Join,
-    },
-    {
-        path: "/test",
-        name: "testAPIPage",
-        component: booktestPage
-    },
-    {
-      path: "/cart",
-      name: "cartPage",
-      component: cart
-    }
+  {
+    path: "/",
+    name: "Main",
+    component: Main,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/join",
+    name: "Join",
+    component: Join,
+  },
+  {
+    path: "/test",
+    name: "testAPIPage",
+    component: booktestPage,
+  },
+  {
+    path: "/cart",
+    name: "cartPage",
+    component: cart,
+  },
+  {
+    path: "/result/:orderNum",
+    name: "resultpage",
+    component: resultpage,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-    scrollBehavior() {
-        return { top: 0 };
-      },
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 export default router;
