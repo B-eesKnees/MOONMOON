2f9cdd7a193324f25dc192de35bbf4a9c02883bf
west-play <81891300+west-play@users.noreply.github.com>
2023-08-07 09:48:31 +0900

    bookInsert.css 약간 더 수정

--- vue_front/src/views/bookInsert.vue
+++ vue_front/src/views/bookInsert.vue
@@ -63,7 +63,7 @@
         <div class="contents">{{ book_pubdate }}</div>
       </div>
       <div class="con_line">
-        <h6 class="line">알라딘<br />판매지수:</h6>
+        <h6 class="line bi_h6">알라딘<br />판매지수:</h6>
         <div class="contents">{{ book_salesPoint }}</div>
       </div>
       <div class="con_line">
