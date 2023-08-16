4635e052cbc511cce3d87e061a12e819a73b007d
jjanggu1 <sdj970514@naver.com>
2023-08-16 16:01:41 +0900

    플로팅

--- node_back/routes/floating.js
+++ node_back/routes/floating.js
@@ -24,7 +24,7 @@ router.get("/recentbook", (req, res) => {
   if (userEmail) {
     // 회원인 경우에는 회원의 최근 본 책 목록 조회
     query =
-      "SELECT rb.REC_VIEWED_AT, b.BOOK_TITLE, b.BOOK_COVER, b.BOOK_AUTHOR, b.BOOK_PRICE " +
+      "SELECT rb.REC_VIEWED_AT, b.BOOK_TITLE, b.BOOK_COVER, b.BOOK_AUTHOR, b.BOOK_PRICE, b.BOOK_ID " +
       "FROM recentbook rb " +
       "INNER JOIN book b ON rb.REC_BOOK_ID = b.BOOK_ID " +
       "WHERE rb.REC_USER_EMAIL=? ORDER BY rb.REC_VIEWED_AT DESC LIMIT 10";
@@ -132,7 +132,7 @@ router.get("/likebook", (req, res) => {
   }
 
   const query =
-    "SELECT lb.LIKE_CREATED_AT, b.BOOK_TITLE, b.BOOK_COVER, b.BOOK_AUTHOR, b.BOOK_PRICE " +
+    "SELECT lb.LIKE_CREATED_AT, b.BOOK_TITLE, b.BOOK_COVER, b.BOOK_AUTHOR, b.BOOK_PRICE, b.BOOK_ID " +
     "FROM likedbook lb " +
     "INNER JOIN book b ON lb.LIKE_BOOK_ID = b.BOOK_ID " +
     "WHERE lb.LIKE_USER_EMAIL=? ORDER BY lb.LIKE_CREATED_AT DESC LIMIT 10";
