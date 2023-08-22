2af0d653f18542926f89af30498dc9d38c46d4b3
minkyeong2 <whitemk4949@gmail.com>
2023-08-22 10:57:14 +0900

    mypage.js/ index.js/ mypageUserInfo.vue

---
+++ vue_front/src/views/mypageUserInfo.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="mypage_menu">
+        <myPageSide />
+    </div>
+    <div class="right_box">
+        <h2 class="userUpdateTitle">회원정보 관리</h2>
+        <hr style="margin-top: 30px; margin-bottom: 50px" />
+
+        <form @submit.prevent="updateUserInfo">
+            <div>
+                <label for="email">아이디</label>
+                <input type="text" id="email" v-model="updatedFields.email" :readonly="true" />
+            </div>
+            <div>
+                <label for="name">이름</label>
+                <input type="text" id="name" v-model="updatedFields.name" :readonly="true" />
+            </div>
+            <div>
+                <label for="password">새 비밀번호</label>
+                <input type="password" id="password" v-model="updatedFields.password" />
+            </div>
+            <div>
+                <label for="sex">성별</label>
+                <input type="text" id="sex" v-model="updatedFields.sex" :readonly="true" />
+            </div>
+            <div>
+                <label for="age">나이</label>
+                <input type="number" id="age" v-model="updatedFields.age" :readonly="true" />
+            </div>
+            <div>
+                <label for="add1">주소</label>
+                <input type="text" id="add1" v-model="updatedFields.add1" />
+            </div>
+            <div>
+                <label for="add2">상세주소</label>
+                <input type="text" id="add2" v-model="updatedFields.add2" />
+            </div>
+            <div>
+                <label for="zipcode">우편번호</label>
+                <input type="text" id="zipcode" v-model="updatedFields.zipcode" />
+            </div>
+            <div>
+                <label for="phone_num">전화번호</label>
+                <input type="text" id="phone_num" v-model="updatedFields.phone_num" />
+            </div>
+
+            <div>
+                <button type="button" @click="cancelUpdate">취소</button>
+                <button type="submit">수정</button>
+            </div>
+        </form>
+        <div v-if="message">{{ message }}</div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+import myPageSide from "../components/myPageSide.vue";
+
+export default {
+    components: { myPageSide },
+    data() {
+        return {
+            originalData: {}, // 기존 데이터를 저장할 객체
+            updatedFields: {
+                email: "",
+                name: "",
+                password: "",
+                sex: "",
+                age: 0,
+                add1: "",
+                add2: "",
+                zipcode: "",
+                phone_num: "",
+            },
+            message: "",
+        };
+    },
+    created() {
+        const email = localStorage.getItem("userID");
+        this.fetchUserInfo(email); // 컴포넌트 생성 시 기존 데이터를 불러오는 메소드 호출
+    },
+    methods: {
+        async fetchUserInfo(email) {
+            try {
+                const response = await axios.get("mypage/getUserInfo", {
+                    params: { userEmail: email },
+                });
+                this.originalData = response.data; // 기존 데이터를 originalData에 저장
+                this.updatedFields = { ...this.originalData }; // 수정할 정보를 updatedFields에 복사
+            } catch (error) {
+                console.error("기존 회원정보 불러오기 오류:", error);
+            }
+        },
+        async updateUserInfo() {
+            const email = localStorage.getItem("userID");
+
+            // POST 요청을 보내기 위한 데이터 구성
+            const requestData = {
+                email: email,
+                updatedFields: {
+                    add1: this.updatedFields.add1,
+                    add2: this.updatedFields.add2,
+                    zipcode: this.updatedFields.zipcode,
+                    phone_num: this.updatedFields.phone_num,
+                    password: this.updatedFields.password,
+                },
+            };
+
+            try {
+                const response = await axios.post("mypage/updateUserInfo", requestData);
+                this.message = response.data.message;
+            } catch (error) {
+                console.error("회원정보 수정 오류:", error);
+                this.message = "회원정보 수정에 실패했습니다.";
+            }
+        },
+    },
+};
+</script>
+<style scoped>
+.userUpdateTitle {
+    font-weight: bold;
+    font-size: 30px;
+}
+</style>
