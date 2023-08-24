7e501d3a4b944b2857226fac7f35a75fdeb0185d
jb3757 <bj3757@gmail.com>
2023-08-24 15:16:56 +0900

    qna

--- vue_front/src/views/qnaEdit.vue
+++ vue_front/src/views/qnaEdit.vue
@@ -1,93 +1,93 @@
 <style src="../assets/css/qnaEdit.css"></style>
 <template>
-  <div>
-    <GnbBar />
+    <div>
+        <GnbBar />
 
-    <div class="mypage_menu"></div>
-    <div class="right_box">
-      <h1 class="qna_title_big">1:1 문의 수정</h1>
-      <div class="write_box">
-        <div class="title_write">
-          <h2 class="title_text">제목</h2>
-          <input class="title_input" type="text" v-model="this.qna.qna_title">
+        <div class="mypage_menu"></div>
+        <div class="right_box">
+            <h1 class="qna_title_big">1:1 문의 수정</h1>
+            <div class="write_box">
+                <div class="title_write">
+                    <h2 class="title_text">제목</h2>
+                    <input class="title_input" type="text" v-model="this.qna.qna_title" maxlength="20" />
+                </div>
+                <div class="con_write">
+                    <h2 class="con_text">내용</h2>
+                    <textarea class="con_input" type="text" v-model="this.qna.qna_con" maxlength="500" />
+                </div>
+            </div>
+            <div class="btn_box">
+                <button class="btn_cancel">취소</button>
+                <button class="btn_accept" @click="qnaEditData">문의 수정</button>
+            </div>
         </div>
-        <div class="con_write">
-          <h2 class="con_text">내용</h2>
-          <input class="con_input" type="text" v-model="this.qna.qna_con">
-        </div>
-      </div>
-      <div class="btn_box">
-        <button class="btn_cancel">취소</button>
-        <button class="btn_accept" @click="qnaEditData">문의 수정</button>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-import GnbBar from '../components/gnbBar.vue'
+import GnbBar from "../components/gnbBar.vue";
 
 export default {
-  components: { 
-    GnbBar 
-  },
-  data() {
-    return {
-      qna: {
-        email: "",
-        qna_id: "",
-        qna_title: "",
-        qna_con: "",
-        qna_rep: 0
-      },
-      qna_id: "",
-    }
-  },
-  created() {
-    this.qna.email = localStorage.getItem("userID");
-    this.qna.qna_id = this.$route.params.id; // 파라미터에서 아이디 받아오기
-    this.getOriginalData();
-  },
-  methods: {
-    getOriginalData() {
-      axios({
-          url: "/qna/qnaOriginalData",
-          method: "post",
-          data: { QNA_ID: this.qna.qna_id },
-      }).then((res) => {
-        this.qna.qna_title = res.data[0].QNA_TITLE;
-        this.qna.qna_con = res.data[0].QNA_CON;
-      });
+    components: {
+        GnbBar,
     },
-    async qnaEditData() {
-      if(!this.qna.qna_title || !this.qna.qna_con) {
-        alert("제목과 내용을 모두 입력해주세요.");
-        return;
-      }
+    data() {
+        return {
+            qna: {
+                email: "",
+                qna_id: "",
+                qna_title: "",
+                qna_con: "",
+                qna_rep: 0,
+            },
+            qna_id: "",
+        };
+    },
+    created() {
+        this.qna.email = localStorage.getItem("userID");
+        this.qna.qna_id = this.$route.params.id; // 파라미터에서 아이디 받아오기
+        this.getOriginalData();
+    },
+    methods: {
+        getOriginalData() {
+            axios({
+                url: "/qna/qnaOriginalData",
+                method: "post",
+                data: { QNA_ID: this.qna.qna_id },
+            }).then((res) => {
+                this.qna.qna_title = res.data[0].QNA_TITLE;
+                this.qna.qna_con = res.data[0].QNA_CON;
+            });
+        },
+        async qnaEditData() {
+            if (!this.qna.qna_title || !this.qna.qna_con) {
+                alert("제목과 내용을 모두 입력해주세요.");
+                return;
+            }
 
-      const editData = {
-        qna_title: this.qna.qna_title,
-        qna_con: this.qna.qna_con,
-        qna_id: this.qna.qna_id,
-      }
-      axios({
-          url: "/qna/qnaEdit",
-          method: "post",
-          data: editData,
-      }).then((res) => {
-        console.log(editData);
-        if (res.data) {
-          alert('문의 내용이 수정되었습니다.');
-          console.log(editData);
-          this.$router.push({ path: '/qna' });          
-          console.log('yeah');
-        } else {
-          alert('문의 수정에 실패했습니다.\n다시 시도해주세요.');
-        }
-      });
-      /* if(!this.qna.qna_title || !this.qna.qna_con) {
+            const editData = {
+                qna_title: this.qna.qna_title,
+                qna_con: this.qna.qna_con,
+                qna_id: this.qna.qna_id,
+            };
+            axios({
+                url: "/qna/qnaEdit",
+                method: "post",
+                data: editData,
+            }).then((res) => {
+                console.log(editData);
+                if (res.data) {
+                    alert("문의 내용이 수정되었습니다.");
+                    console.log(editData);
+                    this.$router.push({ path: "/qna" });
+                    console.log("yeah");
+                } else {
+                    alert("문의 수정에 실패했습니다.\n다시 시도해주세요.");
+                }
+            });
+            /* if(!this.qna.qna_title || !this.qna.qna_con) {
         alert("제목과 내용을 모두 입력해주세요.");
         return;
       }
@@ -111,8 +111,8 @@ export default {
       .catch((err) => {
         console.log(err);
       }); */
-    },
-    /* async qnaEdit() {
+        },
+        /* async qnaEdit() {
       const editData = {
         QNA_TITLE: this.qna_title,
         QNA_CON: this.qna_con,
@@ -127,7 +127,6 @@ export default {
             console.error("Error deleting Q&A", error);
           });
     }, */
-  }
-  
-}
+    },
+};
 </script>
--- vue_front/src/views/qnaWrite.vue
+++ vue_front/src/views/qnaWrite.vue
@@ -1,65 +1,64 @@
 <style src="../assets/css/qnaWrite.css"></style>
 <template>
-  <div>
-    <GnbBar />
+    <div>
+        <GnbBar />
 
-    <div class="mypage_menu"></div>
-    <div class="right_box">
-      <h1 class="qna_title_big">1:1 문의 접수</h1>
-      <div class="write_box">
-        <div class="title_write">
-          <h2 class="title_text">제목</h2>
-          <input class="title_input" type="text" v-model="qna.qna_title">
+        <div class="mypage_menu"></div>
+        <div class="right_box">
+            <h1 class="qna_title_big">1:1 문의 접수</h1>
+            <div class="write_box">
+                <div class="title_write">
+                    <h2 class="title_text">제목</h2>
+                    <input class="title_input" type="text" v-model="qna.qna_title" maxlength="20" />
+                </div>
+                <div class="con_write">
+                    <h2 class="con_text">내용</h2>
+                    <textarea class="con_input" type="text" v-model="qna.qna_con" maxlength="500" />
+                </div>
+            </div>
+            <div class="btn_box">
+                <button class="btn_cancel">취소</button>
+                <button class="btn_accept" @click="qnaWrite">문의 접수</button>
+            </div>
         </div>
-        <div class="con_write">
-          <h2 class="con_text">내용</h2>
-          <input class="con_input" type="text" v-model="qna.qna_con">
-        </div>
-      </div>
-      <div class="btn_box">
-        <button class="btn_cancel">취소</button>
-        <button class="btn_accept" @click="qnaWrite">문의 접수</button>
-      </div>
     </div>
-  </div>
-
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-import GnbBar from '../components/gnbBar.vue'
+import GnbBar from "../components/gnbBar.vue";
 
 export default {
-  components: { 
-    GnbBar 
-  },
-  data() {
-    return {
-      qna: {
-        email: "",
-        qna_title: "",
-        qna_con: "",
-        qna_rep: 0
+    components: {
+        GnbBar,
     },
-    }
-  },
-  created() {
-    this.qna.email = localStorage.getItem("userID");
-  },
-  methods: {
-    qnaWrite() {
-      axios.post("/qna/qnaWrite", this.qna) // this.qna 객체를 서버로 전송
-        .then(response => {
-          // 서버 응답 처리
-          // ... (원하는 처리 방식으로 추가)
-          this.$router.push("/qna"); // 문의 작성 후에 목록 페이지로 이동
-        })
-        .catch(error => {
-          console.error("Error writing Q&A:", error);
-        });
+    data() {
+        return {
+            qna: {
+                email: "",
+                qna_title: "",
+                qna_con: "",
+                qna_rep: 0,
+            },
+        };
     },
-  }
-  
-}
+    created() {
+        this.qna.email = localStorage.getItem("userID");
+    },
+    methods: {
+        qnaWrite() {
+            axios
+                .post("/qna/qnaWrite", this.qna) // this.qna 객체를 서버로 전송
+                .then((response) => {
+                    // 서버 응답 처리
+                    // ... (원하는 처리 방식으로 추가)
+                    this.$router.push("/qna"); // 문의 작성 후에 목록 페이지로 이동
+                })
+                .catch((error) => {
+                    console.error("Error writing Q&A:", error);
+                });
+        },
+    },
+};
 </script>
